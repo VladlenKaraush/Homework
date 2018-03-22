9a0ed3d81ca81f19c29e3e85c4fc8257831e2a7f
vladlen <vladlenkaraush@gmail.com>
2018-03-22 19:54:24 +0300

    Убрал лишние файлы

--- hw07/src/main/java/ru/sberbank/homework/common/UnknownCityException.java
+++
@@ -1,7 +0,0 @@
-package ru.sberbank.homework.common;
-
-class UnknownCityException extends RuntimeException {
-    UnknownCityException(String name) {
-        super("Unknown city with name: " + name);
-    }
-}
--- hw13.2/src/main/java/ru/sberbank/homework/common/ThreadPool.java
+++
@@ -1,8 +0,0 @@
-package ru.sberbank.homework.common;
-
-public interface ThreadPool {
-
-    void start();
-
-    void execute(Runnable runnable);
-}
